a8315d6da9df98d94f66eaeb1f036319b216b762
Sergei Prutsakov <psn206@mail.ru>
2022-05-20 21:32:49 +0300

    add project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("101","011"));
+        System.out.println(bins.mult("101","011"));
+    }
+}
